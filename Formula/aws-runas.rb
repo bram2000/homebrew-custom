47836cedd58361f8262776190abb90c29d91f3d2
Jon Bramley <jon.bramley@digital.hmrc.gov.uk>
2019-07-23 14:33:50 +0100

    Reorder as per instructions from brew

--- Formula/aws-runas.rb
+++ Formula/aws-runas.rb
@@ -4,16 +4,16 @@ class AwsRunas < Formula
   url "https://github.com/mmmorris1975/aws-runas/archive/1.3.4.tar.gz"
   sha256 "4b1e3fe48e470d0705c0d3064aaa0fbb32086976b007137478e5a8499053ab77"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "17be8dd502372ef5c441569bfff6b5f286f313436d6f7195a35032bf199f1304" => :mojave
+  end
+
   depends_on "go" => :build
 
   def install
     system "make"
     bin.install "aws-runas"
-  end
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "17be8dd502372ef5c441569bfff6b5f286f313436d6f7195a35032bf199f1304" => :mojave
   end
 
   test do
