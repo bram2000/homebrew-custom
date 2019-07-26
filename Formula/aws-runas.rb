d30a195656b4606fa2234d2451d6033687644344
Jon Bramley <jon.bramley@digital.hmrc.gov.uk>
2019-07-26 11:17:33 +0100

    Use auto-parsed version

--- Formula/aws-runas.rb
+++ Formula/aws-runas.rb
@@ -1,15 +1,14 @@
 class AwsRunas < Formula
   desc "Friendly way to do AWS STS AssumeRole operations"
   homepage "https://github.com/mmmorris1975/aws-runas/"
-  version "1.3.4"
-  url "https://github.com/mmmorris1975/aws-runas/archive/#{version}.tar.gz"
+  url "https://github.com/mmmorris1975/aws-runas/archive/1.3.4.tar.gz"
   sha256 "4b1e3fe48e470d0705c0d3064aaa0fbb32086976b007137478e5a8499053ab77"
 
   bottle do
     root_url "https://dl.bintray.com/bram2000/bottles-custom"
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "8ad4bd4c1b1a7a15f5ed4b2858316e21f51cd85b59eb29ae072472243879c65b" => :mojave
+    rebuild 2
+    sha256 "984bf903ebb3f2e03555dc4cbef19b63c63f415abb55a4fb16ddbcb046c2c9cd" => :mojave
   end
 
   depends_on "go" => :build
