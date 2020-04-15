9add5ff166327d0a62c8a5d87eb3f12272900e53
Jon Bramley <jon.bramley@sainsburys.co.uk>
2020-04-15 16:14:32 +0100

    Bump to 2.0.5

--- Formula/aws-runas.rb
+++ Formula/aws-runas.rb
@@ -1,13 +1,13 @@
 class AwsRunas < Formula
   desc "Friendly way to do AWS STS AssumeRole operations"
   homepage "https://github.com/mmmorris1975/aws-runas/"
-  url "https://github.com/mmmorris1975/aws-runas/archive/1.5.0.tar.gz"
-  sha256 "1071a2e08f45dd0a21afe2bc77c3605722d406cd293536a6775ad232187dea13"
+  url "https://github.com/mmmorris1975/aws-runas/archive/2.0.5.tar.gz"
+  sha256 "d00c9d947f4270e7c0e6e6c47a2f2ad89dc4b2d0f3adccdb2e541a449a6bb87c"
 
   bottle do
     root_url "https://dl.bintray.com/bram2000/bottles-custom"
     cellar :any_skip_relocation
-    sha256 "d091934658ea6e9abf03c3ab8b105268e42109e17224ceba1f3ed20660a6c6be" => :mojave
+    sha256 "1d59bd8b8fc617a0636311d28ffd5e8304ae630ec6a8e896006ca28d4fa15fa5" => :mojave
   end
 
   depends_on "go" => :build
